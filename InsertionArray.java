67bfa8c374bd8023e4402acaee9bad1fd3425616
“[Coderkreet]” <[krunalbende2@gmail.com]>
2024-02-23 22:51:01 +0530

    2nd Day

---
+++ InsertionArray.java
@@ -0,0 +1,14 @@
+/**
+ * InsertionArray
+ */
+public class InsertionArray {
+
+    public static void main(String[] args) {
+        
+        int arr[]= {1,2,3,4};
+        int size = arr.length;
+
+        System.out.println(size);
+
+    }
+}
