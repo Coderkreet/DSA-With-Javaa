b0b6874d173bf89445a635c1d3c2deb97093a073
“[Coderkreet]” <[krunalbende2@gmail.com]>
2024-02-24 23:54:47 +0530

    3ed Day

--- InsertionArray.java
+++ InsertionArray.java
@@ -3,12 +3,25 @@
  */
 public class InsertionArray {
 
+    public static void ArrayInsertionFirst(int arr[] ,int size , int data){
+        int[] temp = new int[size+1];
+        for (int i = 0; i < size; i++) {
+           temp[i+1] = arr[i];
+
+        }
+        temp[0] = data;
+
+        for (int j = 0; j < temp.length; j++) {
+            System.out.println(temp[j]);
+        }
+        
+    }
     public static void main(String[] args) {
         
         int arr[]= {1,2,3,4};
         int size = arr.length;
-
-        System.out.println(size);
+        int data = 23;
+      ArrayInsertionFirst(arr, size, data);
 
     }
 }
