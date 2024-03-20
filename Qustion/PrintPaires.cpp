5cb484efea200e913581d33d59878ad575a8c514
“[Coderkreet]” <[krunalbende2@gmail.com]>
2024-03-20 20:19:37 +0530

    13

---
+++ Qustion/PrintPaires.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace ::std;
+
+void PrintPair(int arr[] , int n){
+
+for (int i = 0; i < n; i++)
+{
+    
+    for (int j = i; j < n; j++)
+    {
+        cout<<"["<<arr[i]<<" , "<<arr[j]<<"]"<<endl;
+    }
+    
+cout<<" "<<endl;
+}
+
+
+
+}
+
+int main(){
+
+int arr[] = {10,20,30};
+int size = sizeof(arr)/sizeof(int);
+
+PrintPair(arr , size);
+}
---
+++ Qustion/UniqueEle.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace ::std;
+
+void GetUniqueElement(int arr[] , int n){
+    int ans = 0;
+for (int i = 0; i < n; i++)
+{
+    ans = ans ^ arr[i];
+    
+}
+
+cout<<ans;
+}
+
+int main(){
+
+int arr[] ={1,2,3,4,1,2,3};
+int size = sizeof(arr)/sizeof(int);
+
+GetUniqueElement(arr, size);
+
+
+}
